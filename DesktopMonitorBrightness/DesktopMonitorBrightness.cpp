2bb81f5a8c1729ba7b46fcc5085e3abdee72de33
Nixes <callum@see-bry.com>
2016-07-08 13:55:03 +1000

    replace printf's with cout where possible
    old habits die hard

--- DesktopMonitorBrightness/DesktopMonitorBrightness.cpp
+++ DesktopMonitorBrightness/DesktopMonitorBrightness.cpp
@@ -38,7 +38,7 @@ void PrintMonitorBrightness(HANDLE physmonitor) {
 
 	bool bSuccess = GetMonitorBrightness(physmonitor, &min, &current, &max);
 
-	printf("Monitor Brightness values {min %d, current %d, max %d }\n", min, current, max);
+	std::cout << "Monitor Brightness values{ min: " << min << ", :current " << current << ", max: " << max << "}\n";
 }
 
 bool AddMonitorScaleFactor(HANDLE physmonitor) {
@@ -52,13 +52,14 @@ bool AddMonitorScaleFactor(HANDLE physmonitor) {
 	if (bSuccess) {
 		brightnessScaleFactor = (float)max / 100;
 		currentScaledBrightness = (float)current / brightnessScaleFactor;
-		printf("Monitor Brightness values {min %d, current %d, max %d, scalefactor %g, currentscaled %g }\n", min, current, max, brightnessScaleFactor, currentScaledBrightness);
+		std::cout << "Monitor Brightness values {min " << min << ", current" << current << ", max" << max << ", scalefactor" << brightnessScaleFactor << "currentscaled" << currentScaledBrightness << "}\n";
+
 		monitorBrightnessScaleFactor.push_back(brightnessScaleFactor);
 		currentMonitorBrightness.push_back(currentScaledBrightness);
 		return true;
 	}
 	else {
-		printf("Failed to obtain monitor brightness settings!\n This can be caused by querying the current brightness of a given display too soon after it was last read or changed. Or by monitors that don't support this property.\n");
+		std::cout << "Failed to obtain monitor brightness settings!\n This can be caused by querying the current brightness of a given display too soon after it was last read or changed. Or by monitors that don't support this property.\n";
 		return false;
 	}
 }
@@ -81,11 +82,11 @@ static BOOL CALLBACK MonitorEnum(HMONITOR hMon, HDC hdc, LPRECT lprcMonitor, LPA
 		if (pPhysicalMonitors != NULL) {
 			printf("Found %d physical monitors in this HMONITOR\n",numberPhysicalMonitors);
 		} else {
-			printf("no physical monitors found or could not be counted\n");
+			std::cout << "no physical monitors found or could not be counted\n";
 		}
 	}
 	else {
-		printf("failed to get any monitors ");
+		std::cout << "failed to get any monitors ";
 		printf("error: %d \n", GetLastError());
 	}
 
@@ -124,8 +125,7 @@ void setBrightness(HANDLE hMonitor, int brightness, float scalefactor) {
 
 void setAllMonitorsBrightness(int brightness) {
 	// will set the brightness of all detected monitors
-	printf("Setting all monitor brightness to: %i % \n",brightness);
-
+	std::cout << "Setting all monitor brightness to: " << brightness << "\n";
 	// iterate the vector of monitor handles, and for each monitor set the brightness
 	for (unsigned int i = 0; i < physicalMonitorHandles.size();i++) {
 		// according to ms docs, this takes 50ms to return
@@ -160,7 +160,7 @@ float GetFloatHoursNow() {
 		localtime_s(&tm_now,&now);
 		currTimeHours = (float)tm_now.tm_hour + ((float)tm_now.tm_min / 60.0);
 	} catch (int e) {
-		printf( "An exception occurred. Exception Nr. d% \n",e);
+		std::cout << "An exception occurred. Exception: " << e << "\n";
 	}
 	return currTimeHours;
 }
@@ -179,14 +179,18 @@ float GetSunTimeRatio(settings current_settings) {
 	else if (ratio > 1) {
 		ratio = 1;
 	}
-	printf("DEBUG: Set Time Of Day {sunrisetime: %g, sunsettime %g, lightrange: %g, currenttime: %g, ratio: %g}\n", current_settings.sunrise, current_settings.sunset, lightrange, currenttime, ratio);
+	std::cout << "DEBUG: Set Time Of Day {sunrisetime: " << current_settings.sunrise 
+		<< ", sunsettime: " << current_settings.sunset
+		<< ", lightrange: " << lightrange 
+		<< ", currenttime: " << currenttime 
+		<< ", ratio: " << ratio << "}\n";
 	return ratio;
 }
 
 void SetBasedOnTimeOfDay(settings current_settings) {
 	while (true) {
 		int sineresult = round(sin(GetSunTimeRatio(current_settings)  * PI) * 100);
-		printf("Sine func result: %i\n", sineresult);
+		std::cout << "Sine func result: " << sineresult << "\n";
 
 		SetBrightnessFade(sineresult, currentMonitorBrightness[0]);
 		Sleep(1000 * current_settings.polling_time);
@@ -262,18 +266,12 @@ settings RestoreSettings(std::string settings_location) {
 	return new_settings;
 }
 
-int main(int argc, const char* argv[]) {
-	printf("DesktopMonitorBrightness, to use include arg1 brightness as a value between 0 and 100\n");
+int main(int argc, const char* argv[]) {;
+	std::cout << "DesktopMonitorBrightness, to use include arg1 brightness as a value between 0 and 100\n";
+	settings current_settings = RestoreSettings("settings.json");
 
 	getMonitorHandles();
 
-	settings current_settings = RestoreSettings("settings.json");
-
-
-	//SaveTextFile("test.txt", "some test text\n and all\n that\n shit");
-	//printf("Text File contents %s",LoadTextFile("test.txt"));
-
-	//printf("Float hours %g \n", GetFloatHoursNow());
 	SetBasedOnTimeOfDay(current_settings);
 
 	if (argc > 1) {
@@ -284,7 +282,7 @@ int main(int argc, const char* argv[]) {
 			//setAllMonitorsBrightness(brightness);
 			SetBrightnessFade(brightness, currentMonitorBrightness[0]);
 		} else {
-			printf("Unable to parse brightness argument!\n");
+			std::cout << "Unable to parse brightness argument!\n";
 			return 1;
 		}
 	}
