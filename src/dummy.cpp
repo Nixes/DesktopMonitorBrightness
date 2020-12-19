ef7197a37ecd7323bef72350cbee7d4490c71416
Callum Bryant <callum.l.bryant@gmail.com>
2020-12-19 12:07:26 +1100

    add dummy main as required for unreliable conan logic

---
+++ src/dummy.cpp
@@ -0,0 +1,12 @@
+//
+// Created by callum bryant on 19/12/2020.
+//
+// Required by https://github.com/conan-io/conan-center-index/issues/1245#issuecomment-608102318
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+
+    return 1;
+}
